b5fa026477837a8e1a3944a486b660ca30ab0324
Artak1999 <dashyan.1999@mail.ru>
2022-07-05 18:07:02 +0500

    Easy examples.

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result = first > second ? first : second;
+        if(result == first)
+            result = 10;
+        else
+            result = -10;
+        System.out.println(result);
     }
 }
