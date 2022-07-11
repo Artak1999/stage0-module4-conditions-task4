bf227da48b41e22c93d400d72e47ec59a88d49c4
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 21:09:38 +0500

    Easy examples.

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -3,6 +3,11 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
         int result = third > (first > second ? first : second) ? third : ((first > second) ? first : second);
-        System.out.println(result);
+        if(result == first)
+            System.out.println(first);
+        else if(result == second)
+            System.out.println(second);
+        else
+            System.out.println(third);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -3,6 +3,9 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
         int result = first > second ? first : second;
-        System.out.println(result);
+        if(result == first)
+            System.out.println(first);
+        else
+            System.out.println(second);
     }
 }
